f40ff24786b22a078d0464c73319817f82a1f2de
Eliot Jones <elioty@hotmail.co.uk>
2017-12-05 23:20:14 +0000

    change script options for app veyor

--- src/CodeCoverage/CodeCoverage.cmd
+++ src/CodeCoverage/CodeCoverage.cmd
@@ -6,10 +6,4 @@ nuget restore packages.config -PackagesDirectory .
 
 cd ..
 
-dotnet restore  UglyToad.Pdf.sln
-dotnet build  UglyToad.Pdf.sln --no-incremental -c debug /p:codecov=true
- 
-rem The -threshold options prevents this taking ages...
-CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test UglyToad.Pdf.Tests\UglyToad.Pdf.Tests.csproj --no-build -c debug" -register:user -output:.\test-results.xml -hideskipped:All -returntargetcode -oldStyle -filter:"+[UglyToad.Pdf*]* -[UglyToad.Pdf.Tests*]*" 
-
-if %errorlevel% neq 0 exit /b %errorlevel%
+CodeCoverage\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"dotnet.exe" -targetargs:"test UglyToad.Pdf.Tests\UglyToad.Pdf.Tests.csproj  --framework netcoreapp2.0 -c debug" -register:user -output:.\test-results.xml -hideskipped:All -returntargetcode -oldStyle -filter:"+[UglyToad.Pdf*]* -[UglyToad.Pdf.Tests*]*" 
