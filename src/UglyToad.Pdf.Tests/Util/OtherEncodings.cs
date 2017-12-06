ede706baae45ccc6afd2b6416391a98cd925a704
Eliot Jones <elioty@hotmail.co.uk>
2017-12-06 00:13:33 +0000

    add very simple tests to boost coverage

---
+++ src/UglyToad.Pdf.Tests/Util/OtherEncodings.cs
@@ -0,0 +1,32 @@
+﻿namespace UglyToad.Pdf.Tests.Util
+{
+    using Pdf.Util;
+    using Xunit;
+
+    public class OtherEncodingsTests
+    {
+        [Fact]
+        public void BytesNullReturnsNullString()
+        {
+            var result = OtherEncodings.BytesAsLatin1String(null);
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void BytesEmptyReturnsEmptyString()
+        {
+            var result = OtherEncodings.BytesAsLatin1String(new byte[0]);
+
+            Assert.Equal(string.Empty, result);
+        }
+
+        [Fact]
+        public void StringNullReturnsNullBytes()
+        {
+            var result = OtherEncodings.StringAsLatin1Bytes(null);
+
+            Assert.Null(result);
+        }
+    }
+}
