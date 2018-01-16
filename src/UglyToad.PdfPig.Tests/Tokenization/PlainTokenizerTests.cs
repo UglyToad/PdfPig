69736d80cbe7babe0e342b828bea0a397d5fef15
Eliot Jones <elioty@hotmail.co.uk>
2018-01-16 08:00:52 +0000

    add plain tokenizer tests for uncovered lines and add editorconfig

---
+++ src/UglyToad.PdfPig.Tests/Tokenization/PlainTokenizerTests.cs
@@ -0,0 +1,35 @@
+﻿namespace UglyToad.PdfPig.Tests.Tokenization
+{
+    using PdfPig.Tokenization;
+    using PdfPig.Tokenization.Tokens;
+    using Xunit;
+
+    public class PlainTokenizerTests
+    {
+        private readonly PlainTokenizer tokenizer = new PlainTokenizer();
+
+        [Fact]
+        public void TextNullReturnsNullToken()
+        {
+            var input = StringBytesTestConverter.Convert("null ");
+
+            var result = tokenizer.TryTokenize(input.First, input.Bytes, out var token);
+
+            Assert.True(result);
+
+            Assert.Equal(NullToken.Instance, token);
+        }
+
+        [Fact]
+        public void TryTokenizeWhitespaceFalse()
+        {
+            var input = StringBytesTestConverter.Convert("    something");
+
+            var result = tokenizer.TryTokenize(input.First, input.Bytes, out var token);
+
+            Assert.False(result);
+
+            Assert.Null(token);
+        }
+    }
+}
