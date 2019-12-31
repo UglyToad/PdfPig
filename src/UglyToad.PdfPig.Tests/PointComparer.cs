af9bca925ea32c21a7b4a4002cc1a75687a74fe0
Eliot Jones <elioty@hotmail.co.uk>
2019-12-31 14:07:52 +0000

    fix tests by using custom equality comparers
    since we now round glyph widths for truetype fonts in the widths array of the pdf some values are out by a very small amount from the expected value. since we don't care about such fractional inaccuracy we use a custom comparer for these tests.

---
+++ src/UglyToad.PdfPig.Tests/PointComparer.cs
@@ -0,0 +1,27 @@
+﻿namespace UglyToad.PdfPig.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using PdfPig.Geometry;
+
+    internal class PointComparer : IEqualityComparer<PdfPoint>
+    {
+        private readonly IEqualityComparer<double> doubleComparer;
+
+        public PointComparer(IEqualityComparer<double> doubleComparer)
+        {
+            this.doubleComparer = doubleComparer ?? throw new ArgumentNullException(nameof(doubleComparer));
+        }
+
+        public bool Equals(PdfPoint a, PdfPoint b)
+        {
+            return doubleComparer.Equals(a.X, b.X)
+                   && doubleComparer.Equals(a.Y, b.Y);
+        }
+
+        public int GetHashCode(PdfPoint obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
