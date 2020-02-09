ec68231ab3e5309aa41eb2c42d4d1dc877c0e097
BobLd <boris.ledeist@edhec.com>
2020-02-09 17:54:20 +0000

    remove useless using

--- src/UglyToad.PdfPig.Tests/Integration/MarkedContentExtractionTests.cs
+++ src/UglyToad.PdfPig.Tests/Integration/MarkedContentExtractionTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using UglyToad.PdfPig.Content;
+﻿using UglyToad.PdfPig.Content;
 using Xunit;
 
 namespace UglyToad.PdfPig.Tests.Integration
