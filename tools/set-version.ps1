25b6f1d46d847b3fb9d403216cefbc41f7803f92
Eliot Jones <elioty@hotmail.co.uk>
2020-02-27 13:29:15 +0000

    change set version script to use correct path
    rather than directory relative to the working directory use the path relative to the tools folder.

--- tools/set-version.ps1
+++ tools/set-version.ps1
@@ -3,7 +3,9 @@ param (
   [string]$version
 )
 
-$projs = Get-ChildItem "../src" -Recurse | Where-Object { $_.extension -eq ".csproj" -and $_.name.IndexOf("Tests") -lt 0 }
+$root = (Split-Path -parent $PSCommandPath)
+
+$projs = Get-ChildItem "$root/../src" -Recurse | Where-Object { $_.extension -eq ".csproj" -and $_.name.IndexOf("Tests") -lt 0 }
 $projs | ForEach-Object {
   $xml = New-Object XML
   $xml.Load($_.FullName)
