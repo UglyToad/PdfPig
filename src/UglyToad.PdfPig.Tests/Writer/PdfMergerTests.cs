14599521f50cdc2daccb63cc9d4ccb1693aa9ff8
Eliot Jones <elioty@hotmail.co.uk>
2020-03-02 16:20:15 +0000

    test the simple document merge in reverse order
    check we can create the document in both merge orders.

--- src/UglyToad.PdfPig.Tests/Writer/PdfMergerTests.cs
+++ src/UglyToad.PdfPig.Tests/Writer/PdfMergerTests.cs
@@ -29,5 +29,31 @@
                 Assert.Equal("I am a simple pdf.", page2.Text);
             }
         }
+
+        [Fact]
+        public void CanMerge2SimpleDocumentsReversed()
+        {
+            var one = IntegrationHelpers.GetDocumentPath("Single Page Simple - from open office.pdf");
+            var two = IntegrationHelpers.GetDocumentPath("Single Page Simple - from inkscape.pdf");
+
+            var result = PdfMerger.Merge(one, two);
+
+            System.IO.File.WriteAllBytes(@"C:\temp\merged.pdf", result);
+
+            using (var document = PdfDocument.Open(result, ParsingOptions.LenientParsingOff))
+            {
+                Assert.Equal(2, document.NumberOfPages);
+
+                Assert.Equal(1.5m, document.Version);
+
+                var page1 = document.GetPage(1);
+
+                Assert.Equal("I am a simple pdf.", page1.Text);
+
+                var page2 = document.GetPage(2);
+
+                Assert.Equal("Write something inInkscape", page2.Text);
+            }
+        }
     }
 }
