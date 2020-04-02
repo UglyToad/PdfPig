b368be8e9cafb998b2c61a945f1e634892c96d48
BobLd <boris.ledeist@edhec.com>
2020-04-02 16:24:12 +0100

    Moving PdfPath from PdfPig.Core to PdfPig Add FillColor, StrokeColor, LineWidth, LineDashPattern and LineCapStyle to PdfPath.

--- src/UglyToad.PdfPig/Graphics/PdfPath.cs
+++ src/UglyToad.PdfPig/Graphics/PdfPath.cs
@@ -1,6 +1,9 @@
-﻿namespace UglyToad.PdfPig.Core
+﻿namespace UglyToad.PdfPig.Graphics
 {
     using System.Collections.Generic;
+    using UglyToad.PdfPig.Core;
+    using UglyToad.PdfPig.Graphics.Colors;
+    using UglyToad.PdfPig.Graphics.Core;
 
     /// <summary>
     /// A path is made up of one or more disconnected subpaths, each comprising a sequence of connected segments. The topology of the path is unrestricted: it may be concave or convex, may contain multiple subpaths representing disjoint areas, and may intersect itself in arbitrary ways.
@@ -17,6 +20,36 @@
         /// Returns true if this is a clipping path.
         /// </summary>
         public bool IsClipping { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IColor FillColor { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IColor StrokeColor { get; set; }
+
+        /// <summary>
+        /// Thickness in user space units of path to be stroked.
+        /// </summary>
+        public double LineWidth { get; set; } = double.NaN;
+
+        /// <summary>
+        /// The pattern to be used for stroked lines.
+        /// </summary>
+        public LineDashPattern? LineDashPattern { get; set; }
+
+        /// <summary>
+        /// The cap style to be used for stroked lines.
+        /// </summary>
+        public LineCapStyle LineCapStyle { get; set; }
+
+        /// <summary>
+        /// The join style to be used for stroked lines.
+        /// </summary>
+        public LineJoinStyle LineJoinStyle { get; set; }
 
         /// <summary>
         /// Set the clipping mode for this path.
