5a82c366310d1f2df6563ddd0fffce4e34c0cf48
romain v <rvergnory@lucca.fr>
2020-08-17 11:10:44 +0200

    FIX : undefined references is a valid use case.
    I tried to mitigate the breaking change by keep on throwing in most uses of the change method.

--- src/UglyToad.PdfPig/Parser/Parts/DirectObjectFinder.cs
+++ src/UglyToad.PdfPig/Parser/Parts/DirectObjectFinder.cs
@@ -43,9 +43,13 @@
             return false;
         }
 
-        public static T Get<T>(IndirectReference reference, IPdfTokenScanner scanner) where T : IToken
+        public static T Get<T>(IndirectReference reference, IPdfTokenScanner scanner) where T : class, IToken
         {
             var temp = scanner.Get(reference);
+            if (temp is null)
+            {
+                return null;
+            }
 
             if (temp.Data is T locatedResult)
             {
@@ -75,7 +79,7 @@
             throw new PdfDocumentFormatException($"Could not find the object number {reference} with type {typeof(T).Name}.");
         }
 
-        public static T Get<T>(IToken token, IPdfTokenScanner scanner) where T : IToken
+        public static T Get<T>(IToken token, IPdfTokenScanner scanner) where T : class, IToken
         {
             if (token is T result)
             {
@@ -84,32 +88,7 @@
 
             if (token is IndirectReferenceToken reference)
             {
-                var temp = scanner.Get(reference.Data);
-
-                if (temp.Data is T locatedResult)
-                {
-                    return locatedResult;
-                }
-
-                if (temp.Data is IndirectReferenceToken nestedReference)
-                {
-                    return Get<T>(nestedReference, scanner);
-                }
-
-                if (temp.Data is ArrayToken array && array.Data.Count == 1)
-                {
-                    var arrayElement = array.Data[0];
-
-                    if (arrayElement is IndirectReferenceToken arrayReference)
-                    {
-                        return Get<T>(arrayReference, scanner);
-                    }
-
-                    if (arrayElement is T arrayToken)
-                    {
-                        return arrayToken;
-                    }
-                }
+                return Get<T>(reference.Data, scanner);
             }
 
             throw new PdfDocumentFormatException($"Could not find the object {token} with type {typeof(T).Name}.");
