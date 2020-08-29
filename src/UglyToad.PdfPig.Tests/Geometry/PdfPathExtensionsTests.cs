f656922d2d07e7a54f3841b2f016af631c2113de
BobLd <boris.ledeist@edhec.com>
2020-08-29 19:27:12 +0100

    add tests for PdfPath.Contains(Rectangle) - EvenOdd

---
+++ src/UglyToad.PdfPig.Tests/Geometry/PdfPathExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿namespace UglyToad.PdfPig.Tests.Geometry
+{
+    using System.Linq;
+    using UglyToad.PdfPig.Core;
+    using UglyToad.PdfPig.Geometry;
+    using UglyToad.PdfPig.Tests.Integration;
+    using Xunit;
+
+    public class PdfPathExtensionsTests
+    {
+        [Fact]
+        public void ContainsRectangleEvenOdd()
+        {
+            using (var document = PdfDocument.Open(IntegrationHelpers.GetDocumentPath("path_ext_oddeven"),
+                 new ParsingOptions() { ClipPaths = true }))
+            {
+                var page = document.GetPage(1);
+                var words = page.GetWords().ToList();
+
+                foreach (var path in page.ExperimentalAccess.Paths)
+                {
+                    Assert.NotEqual(FillingRule.NonZeroWinding, path.FillingRule); // allow none and even-odd
+
+                    foreach (var c in words.Where(w => path.Contains(w.BoundingBox)).ToList())
+                    {
+                        Assert.Equal("in", c.Text.Split("_").Last());
+                        words.Remove(c);
+                    }
+                }
+
+                foreach (var w in words)
+                {
+                    Assert.NotEqual("in", w.Text.Split("_").Last());
+                }
+            }
+        }
+    }
+}
