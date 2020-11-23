68e487d5ff752be15722f7b991a638344368b97b
BobLd <boris.ledeist@edhec.com>
2020-11-23 12:38:44 +0000

    Add IPageImageRenderer

---
+++ src/UglyToad.PdfPig/Rendering/IPageImageRenderer.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UglyToad.PdfPig.Content;
+
+namespace UglyToad.PdfPig.Rendering
+{
+    /// <summary>
+    /// Render page as an image.
+    /// </summary>
+    public interface IPageImageRenderer
+    {
+        /// <summary>
+        /// Render page as an image.
+        /// </summary>
+        /// <param name="page">The pdf page.</param>
+        /// <param name="scale">The scale to apply to the page (i.e. zoom level).</param>
+        /// <returns>The image as a memory stream.</returns>
+        MemoryStream Render(Page page, double scale);
+    }
+}
