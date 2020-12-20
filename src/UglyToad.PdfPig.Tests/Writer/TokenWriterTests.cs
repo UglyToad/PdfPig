e9c8fbbbaadd7dddd74ffd84f09b3eaf785e59b9
InusualZ <wesmoret98@gmail.com>
2020-12-20 19:34:13 +0000

    Fix TokenWriter::WriteString not escaping properly special characters

---
+++ src/UglyToad.PdfPig.Tests/Writer/TokenWriterTests.cs
@@ -0,0 +1,30 @@
+﻿namespace UglyToad.PdfPig.Tests.Writer
+{
+    using Integration;
+    using PdfPig.Writer;
+    using System.IO;
+    using UglyToad.PdfPig.Tokens;
+    using Xunit;
+
+    public class TokenWriterTests
+    {
+
+        [Fact]
+        public void EscapeSpecialCharacter()
+        {
+            using (var memStream = new MemoryStream())
+            {
+                TokenWriter.WriteToken(new StringToken("\\"), memStream);
+                TokenWriter.WriteToken(new StringToken("(Hello)"), memStream);
+
+                // Read Test
+                memStream.Position = 0;
+                using (var streamReader = new StreamReader(memStream))
+                {
+                    var line = streamReader.ReadToEnd();
+                    Assert.Equal("(\\\\) (\\(Hello\\)) ", line);
+                }
+            }
+        }
+    }
+}
