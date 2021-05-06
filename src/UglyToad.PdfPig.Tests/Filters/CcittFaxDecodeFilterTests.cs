8c6f705d2a8a38b4d715bbfd7605fcfd62caa479
Kasper Frank <kasperdaff@gmail.com>
2021-05-06 21:35:17 +0200

    Basic unit tests of CCITT Fax filter decoding

---
+++ src/UglyToad.PdfPig.Tests/Filters/CcittFaxDecodeFilterTests.cs
@@ -0,0 +1,40 @@
+﻿namespace UglyToad.PdfPig.Tests.Filters
+{
+    using System.Collections.Generic;
+    using UglyToad.PdfPig.Filters;
+    using UglyToad.PdfPig.Tests.Images;
+    using UglyToad.PdfPig.Tokens;
+    using Xunit;
+
+    public class CcittFaxDecodeFilterTests
+    {
+        [Fact]
+        public void CanDecodeCCittFaxCompressedImageData()
+        {
+            var encodedBytes = ImageHelpers.LoadFileBytes("ccittfax-encoded.bin");
+
+            var filter = new CcittFaxDecodeFilter();
+            var dictionary = new Dictionary<NameToken, IToken>
+            {
+                { NameToken.D, new ArrayToken(new []{ new NumericToken(1), new NumericToken(0) })},
+                { NameToken.W, new NumericToken(1800) },
+                { NameToken.H, new NumericToken(3113) },
+                { NameToken.Bpc, new NumericToken(1) },
+                { NameToken.F, NameToken.CcittfaxDecode },
+                { NameToken.DecodeParms,
+                    new DictionaryToken(new Dictionary<NameToken, IToken>
+                    {
+                        { NameToken.K, new NumericToken(-1) },
+                        { NameToken.Columns, new NumericToken(1800) },
+                        { NameToken.Rows, new NumericToken(3113) },
+                        { NameToken.BlackIs1, BooleanToken.True }
+                    })
+                }
+            };
+
+            var expectedBytes = ImageHelpers.LoadFileBytes("ccittfax-decoded.bin");
+            var decodedBytes = filter.Decode(encodedBytes, new DictionaryToken(dictionary), 0);
+            Assert.Equal(expectedBytes, decodedBytes);
+        }
+    }
+}
---
+++ src/UglyToad.PdfPig.Tests/Images/ImageHelpers.cs
@@ -0,0 +1,39 @@
+﻿namespace UglyToad.PdfPig.Tests.Images
+{
+    using System;
+    using System.IO;
+    using UglyToad.PdfPig.Images.Png;
+
+    public static class ImageHelpers
+    {
+        private static readonly string FilesFolder = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Images", "Files"));
+
+        public static byte[] LoadFileBytes(string filename)
+        {
+            return File.ReadAllBytes(Path.Combine(FilesFolder, filename));
+        }
+
+        public static bool ImagesAreEqual(byte[] first, byte[] second)
+        {
+            var png1 = Png.Open(first);
+            var png2 = Png.Open(second);
+            if (png1.Width != png2.Width || png1.Height != png2.Height || png1.HasAlphaChannel != png2.HasAlphaChannel)
+            {
+                return false;
+            }
+
+            for (var y = 0; y < png1.Height; y++)
+            {
+                for (var x = 0; x < png1.Width; x++)
+                {
+                    if (!png1.GetPixel(x, y).Equals(png2.GetPixel(x, y))) 
+                    { 
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
