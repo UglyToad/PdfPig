50dc67c2ca01d231a0958cf44d473fb045f9e271
Eliot Jones <elioty@hotmail.co.uk>
2021-08-14 13:21:27 -0400

    nightly push action rework v5
    include alpha in the generate nightly package version
    i don't know which logic nuget uses to detect pre-release
    so it's safer to include alpha in the nightly build version.

--- tools/generate-nightly-version.ps1
+++ tools/generate-nightly-version.ps1
@@ -2,6 +2,6 @@ $toolsRoot = (Split-Path -parent $PSCommandPath)
 
 $mainVersion = & "$toolsRoot\get-main-version.ps1"
 $commitHash = (git rev-parse HEAD).Substring(0, 5)
-$date = [System.DateTime]::Now.ToString('yyyyMMdd')
-$nightlyVersion = "$mainVersion-$date.$commitHash"
+$date = [System.DateTime]::UtcNow.ToString('yyyyMMdd')
+$nightlyVersion = "$mainVersion-alpha.$date.$commitHash"
 Write-Output $nightlyVersion
