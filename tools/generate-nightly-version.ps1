e1716e039392a60107dedcd419a417eea68091d2
Eliot Jones <elioty@hotmail.co.uk>
2021-08-16 21:40:34 -0400

    nightly push action rework v9

--- tools/generate-nightly-version.ps1
+++ tools/generate-nightly-version.ps1
@@ -3,5 +3,5 @@ $toolsRoot = (Split-Path -parent $PSCommandPath)
 $mainVersion = & "$toolsRoot\get-main-version.ps1"
 $commitHash = (git rev-parse HEAD).Substring(0, 5)
 $date = [System.DateTime]::UtcNow.ToString('yyyyMMdd')
-$nightlyVersion = "$mainVersion-alpha-$date.$commitHash"
+$nightlyVersion = "$mainVersion-alpha-$date`_$commitHash"
 Write-Output $nightlyVersion
