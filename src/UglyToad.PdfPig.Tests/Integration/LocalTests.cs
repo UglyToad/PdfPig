6a32d041a4a7a55d561ae1f707d0d8bd94bfdd5d
Eliot Jones <elioty@hotmail.co.uk>
2022-04-02 15:59:08 -0400

    naughty local tests, go away

--- src/UglyToad.PdfPig.Tests/Integration/LocalTests.cs
+++ src/UglyToad.PdfPig.Tests/Integration/LocalTests.cs
@@ -1,39 +1,39 @@
 ﻿namespace UglyToad.PdfPig.Tests.Integration
 {
-    using System;
-    using System.Diagnostics;
-    using System.IO;
-    using Xunit;
+    //using System;
+    //using System.Diagnostics;
+    //using System.IO;
+    //using Xunit;
 
     /// <summary>
     /// A class for testing files which are not checked in to source control.
     /// </summary>
     public class LocalTests
     {
-        [Fact]
-        public void Tests()
-        {
-            var files = Directory.GetFiles(@"C:\temp\pdfs", "*.pdf");
+        //[Fact]
+        //public void Tests()
+        //{
+        //    var files = Directory.GetFiles(@"C:\temp\pdfs", "*.pdf");
 
-            foreach (var file in files)
-            {
-                try
-                {
-                    using (var document = PdfDocument.Open(file, new ParsingOptions { UseLenientParsing = false }))
-                    {
-                        for (var i = 1; i <= document.NumberOfPages; i++)
-                        {
-                            var page = document.GetPage(i);
-                            var text = page.Text;
-                            Trace.WriteLine(text);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidOperationException($"Error parsing: {Path.GetFileName(file)}.", ex);
-                }
-            }
-        }
+        //    foreach (var file in files)
+        //    {
+        //        try
+        //        {
+        //            using (var document = PdfDocument.Open(file, new ParsingOptions { UseLenientParsing = false }))
+        //            {
+        //                for (var i = 1; i <= document.NumberOfPages; i++)
+        //                {
+        //                    var page = document.GetPage(i);
+        //                    var text = page.Text;
+        //                    Trace.WriteLine(text);
+        //                }
+        //            }
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            throw new InvalidOperationException($"Error parsing: {Path.GetFileName(file)}.", ex);
+        //        }
+        //    }
+        //}
     }
 }
