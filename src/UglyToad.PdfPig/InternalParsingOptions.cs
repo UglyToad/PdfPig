9c9c7c99ea12487e0cdacd1d8073685a0cb22303
Eliot Jones <elioty@hotmail.co.uk>
2022-10-09 16:01:20 -0400

    ci new namespace file scope unsupported

--- src/UglyToad.PdfPig/InternalParsingOptions.cs
+++ src/UglyToad.PdfPig/InternalParsingOptions.cs
@@ -1,34 +1,35 @@
-﻿namespace UglyToad.PdfPig;
-
-using Logging;
-using System.Collections.Generic;
-
-/// <summary>
-/// <see cref="ParsingOptions"/> but without being a public API/
-/// </summary>
-internal class InternalParsingOptions
+﻿namespace UglyToad.PdfPig
 {
-    public IReadOnlyList<string> Passwords { get; }
+    using Logging;
+    using System.Collections.Generic;
 
-    public bool UseLenientParsing { get; }
-
-    public bool ClipPaths { get; }
-
-    public bool SkipMissingFonts { get; }
-
-    public ILog Logger { get; }
-
-    public InternalParsingOptions(
-        IReadOnlyList<string> passwords,
-        bool useLenientParsing,
-        bool clipPaths,
-        bool skipMissingFonts,
-        ILog logger)
+    /// <summary>
+    /// <see cref="ParsingOptions"/> but without being a public API/
+    /// </summary>
+    internal class InternalParsingOptions
     {
-        Passwords = passwords;
-        UseLenientParsing = useLenientParsing;
-        ClipPaths = clipPaths;
-        SkipMissingFonts = skipMissingFonts;
-        Logger = logger;
+        public IReadOnlyList<string> Passwords { get; }
+
+        public bool UseLenientParsing { get; }
+
+        public bool ClipPaths { get; }
+
+        public bool SkipMissingFonts { get; }
+
+        public ILog Logger { get; }
+
+        public InternalParsingOptions(
+            IReadOnlyList<string> passwords,
+            bool useLenientParsing,
+            bool clipPaths,
+            bool skipMissingFonts,
+            ILog logger)
+        {
+            Passwords = passwords;
+            UseLenientParsing = useLenientParsing;
+            ClipPaths = clipPaths;
+            SkipMissingFonts = skipMissingFonts;
+            Logger = logger;
+        }
     }
 }
