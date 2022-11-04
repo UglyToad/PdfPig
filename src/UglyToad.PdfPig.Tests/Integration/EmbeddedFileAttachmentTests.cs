4428a676444febae578a66812be213754329a4f4
Ali Yousefi <ali.visual.studio@gmail.com>
2022-11-04 09:37:04 +0000

    Add Test case to support \r\n in EndText

--- src/UglyToad.PdfPig.Tests/Integration/EmbeddedFileAttachmentTests.cs
+++ src/UglyToad.PdfPig.Tests/Integration/EmbeddedFileAttachmentTests.cs
@@ -21,6 +21,22 @@
         }
 
         [Fact]
+        public void HasCorrectFullPageText()
+        {
+            var path = IntegrationHelpers.GetSpecificTestDocumentPath("paragraph-document.pdf");
+
+            using (var document = PdfDocument.Open(path))
+            {
+                for (var i = 1; i <= document.NumberOfPages; i++)
+                {
+                    var page = document.GetPage(i);
+
+                    Assert.StartsWith("A slightly more complicated solution is necessary is s.Length exceeds Int32.MaxValue. But if\r\nyou need to read a stream that large into memory, you might want to think about a different\r\napproach to your problem.\r\nThis is a text example from Ali Yousefi. Everybody knows this is a test example!\r\n", page.Text);
+                }
+            }
+        }
+
+        [Fact]
         public void HasEmbeddedFiles()
         {
             var path = IntegrationHelpers.GetSpecificTestDocumentPath("embedded-file-attachment.pdf");
