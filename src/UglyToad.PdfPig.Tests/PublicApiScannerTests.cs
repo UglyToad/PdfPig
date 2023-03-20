a7dc2ee19f0c0e6a4fa3b273332a086e507fee6e
mvantzet <mark@radialsg.com>
2023-03-20 14:17:52 +0100

    Added AppearanceStream to public API because it is used in the (public) Annotation constructor

--- src/UglyToad.PdfPig.Tests/PublicApiScannerTests.cs
+++ src/UglyToad.PdfPig.Tests/PublicApiScannerTests.cs
@@ -63,6 +63,7 @@
                 "UglyToad.PdfPig.Annotations.AnnotationBorder",
                 "UglyToad.PdfPig.Annotations.AnnotationFlags",
                 "UglyToad.PdfPig.Annotations.AnnotationType",
+                "UglyToad.PdfPig.Annotations.AppearanceStream",
                 "UglyToad.PdfPig.Annotations.QuadPointsQuadrilateral",
                 "UglyToad.PdfPig.Content.ArtifactMarkedContentElement",
                 "UglyToad.PdfPig.Content.Catalog",
