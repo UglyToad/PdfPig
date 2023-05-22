11df5b520df603eed4cf824c6126033d748a375b
Eliot Jones <elioty@hotmail.co.uk>
2023-05-22 20:40:36 +0100

    update y if preceding letter was blank #603

--- src/UglyToad.PdfPig/Util/DefaultWordExtractor.cs
+++ src/UglyToad.PdfPig/Util/DefaultWordExtractor.cs
@@ -47,6 +47,8 @@
 
                     lettersSoFar.Add(letter);
                     lastLetter = letter;
+                    y = letter.Location.Y;
+                    lastX = letter.Location.X;
                     continue;
                 }
 
