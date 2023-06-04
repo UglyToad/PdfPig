7fe5fc2272613088285ff72becf6b46374110731
Eliot Jones <elioty@hotmail.co.uk>
2023-06-04 14:14:18 +0100

    add test case for armenian alphabet

---
+++ src/UglyToad.PdfPig.Tests/Integration/LittlePigInArmenianTests.cs
@@ -0,0 +1,23 @@
+﻿namespace UglyToad.PdfPig.Tests.Integration;
+
+using System.Linq;
+using Xunit;
+
+public class LittlePigInArmenianTests
+{
+    [Fact]
+    public void CanReadTextCorrectly()
+    {
+        var path = IntegrationHelpers.GetDocumentPath("little-pig-in-armenian.pdf");
+
+        using var document = PdfDocument.Open(path);
+
+        var page = document.GetPage(1);
+
+        var words = page.GetWords().ToList();
+
+        var textFromWords = string.Join(" ", words.Select(x => x.Text));
+
+        Assert.Equal("փոքրիկ խոզ", textFromWords);
+    }
+}
