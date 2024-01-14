4e63e2c415b6aac6f4f19296e8c5fc002b8eeb81
BobLd <38405645+BobLd@users.noreply.github.com>
2024-01-14 20:44:20 +0000

    Add visual verification tests for letter glyphs

---
+++ src/UglyToad.PdfPig.Tests/Integration/VisualVerification/SkiaHelpers/SkiaExtensions.cs
@@ -0,0 +1,97 @@
+﻿namespace UglyToad.PdfPig.Tests.Integration.VisualVerification.SkiaHelpers
+{
+    using PdfPig.Core;
+    using PdfPig.Graphics.Colors;
+    using SkiaSharp;
+    using System;
+    using System.Collections.Generic;
+
+    internal static class SkiaExtensions
+    {
+        public static SKMatrix ToSkMatrix(this TransformationMatrix transformationMatrix)
+        {
+            return new SKMatrix((float)transformationMatrix.A, (float)transformationMatrix.C, (float)transformationMatrix.E,
+                (float)transformationMatrix.B, (float)transformationMatrix.D, (float)transformationMatrix.F,
+                0, 0, 1);
+        }
+
+        public static SKColor ToSKColor(this IColor? pdfColor, decimal alpha)
+        {
+            var color = SKColors.Black;
+            if (pdfColor != null)
+            {
+                var (r, g, b) = pdfColor.ToRGBValues();
+                color = new SKColor(Convert.ToByte(r * 255), Convert.ToByte(g * 255), Convert.ToByte(b * 255));
+            }
+
+            return color.WithAlpha(Convert.ToByte(alpha * 255));
+        }
+
+        public static SKPath ToSKPath(this IReadOnlyList<PdfSubpath> path)
+        {
+            var skPath = new SKPath() { FillType = SKPathFillType.EvenOdd };
+            foreach (var subpath in path)
+            {
+                foreach (var c in subpath.Commands)
+                {
+                    if (c is PdfSubpath.Move move)
+                    {
+                        skPath.MoveTo((float)move.Location.X, (float)move.Location.Y);
+                    }
+                    else if (c is PdfSubpath.Line line)
+                    {
+                        skPath.LineTo((float)line.To.X, (float)line.To.Y);
+                    }
+                    else if (c is PdfSubpath.BezierCurve curve)
+                    {
+                        if (curve.StartPoint.Equals(curve.FirstControlPoint)) // TODO - This needs to be fixed in PdfPig
+                        {
+                            // Quad curve
+                            skPath.QuadTo((float)curve.SecondControlPoint.X, (float)curve.SecondControlPoint.Y,
+                                (float)curve.EndPoint.X, (float)curve.EndPoint.Y);
+                        }
+                        else
+                        {
+                            // Cubic curve
+                            skPath.CubicTo((float)curve.FirstControlPoint.X, (float)curve.FirstControlPoint.Y,
+                                (float)curve.SecondControlPoint.X, (float)curve.SecondControlPoint.Y,
+                                (float)curve.EndPoint.X, (float)curve.EndPoint.Y);
+                        }
+                    }
+                    else if (c is PdfSubpath.Close)
+                    {
+                        skPath.Close();
+                    }
+                }
+            }
+
+            return skPath;
+        }
+
+        public static SKPaintStyle? ToSKPaintStyle(this TextRenderingMode textRenderingMode)
+        {
+            // This is an approximation
+            switch (textRenderingMode)
+            {
+                case TextRenderingMode.Stroke:
+                case TextRenderingMode.StrokeClip:
+                    return SKPaintStyle.Stroke;
+
+                case TextRenderingMode.Fill:
+                case TextRenderingMode.FillClip:
+                    return SKPaintStyle.Fill;
+
+                case TextRenderingMode.FillThenStroke:
+                case TextRenderingMode.FillThenStrokeClip:
+                    return SKPaintStyle.StrokeAndFill;
+
+                case TextRenderingMode.NeitherClip:
+                    return SKPaintStyle.Stroke; // Not correct
+
+                case TextRenderingMode.Neither:
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ src/UglyToad.PdfPig.Tests/Integration/VisualVerification/SkiaHelpers/SkiaGlyphPageFactory.cs
@@ -0,0 +1,40 @@
+﻿namespace UglyToad.PdfPig.Tests.Integration.VisualVerification.SkiaHelpers
+{
+    using Content;
+    using Outline.Destinations;
+    using PdfPig.Core;
+    using PdfPig.Filters;
+    using PdfPig.Geometry;
+    using PdfPig.Graphics.Operations;
+    using PdfPig.Parser;
+    using PdfPig.Tokenization.Scanner;
+    using PdfPig.Tokens;
+    using SkiaSharp;
+    using System.Collections.Generic;
+
+    internal sealed class SkiaGlyphPageFactory : BasePageFactory<SKPicture>
+    {
+        public SkiaGlyphPageFactory(
+            IPdfTokenScanner pdfScanner,
+            IResourceStore resourceStore,
+            ILookupFilterProvider filterProvider,
+            IPageContentParser pageContentParser,
+            ParsingOptions parsingOptions)
+            : base(pdfScanner, resourceStore, filterProvider, pageContentParser, parsingOptions)
+        {
+        }
+
+        protected override SKPicture ProcessPage(int pageNumber, DictionaryToken dictionary, NamedDestinations namedDestinations,
+            MediaBox mediaBox, CropBox cropBox, UserSpaceUnit userSpaceUnit, PageRotationDegrees rotation,
+            TransformationMatrix initialMatrix, IReadOnlyList<IGraphicsStateOperation> operations)
+        {
+            // Special case where cropbox is outside mediabox: use cropbox instead of intersection
+            var effectiveCropBox = new CropBox(mediaBox.Bounds.Intersect(cropBox.Bounds) ?? cropBox.Bounds);
+
+            var context = new SkiaGlyphStreamProcessor(pageNumber, ResourceStore, PdfScanner, PageContentParser,
+                FilterProvider, effectiveCropBox, userSpaceUnit, rotation, initialMatrix, ParsingOptions);
+
+            return context.Process(pageNumber, operations);
+        }
+    }
+}
