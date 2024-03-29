fada171efb7a2a6a83be5ec3fe7cb1015d0842d9
Jason Nelson <jason@carbonmade.com>
2024-03-29 11:13:37 -0700

    Spanify PdfDocEncoding.TryConvertBytesToString

--- src/UglyToad.PdfPig.Core/PdfDocEncoding.cs
+++ src/UglyToad.PdfPig.Core/PdfDocEncoding.cs
@@ -1,5 +1,6 @@
 ﻿namespace UglyToad.PdfPig.Core
 {
+    using System;
     using System.Collections.Generic;
 
     /// <summary>
@@ -263,7 +264,7 @@
         /// Try to convert raw bytes to a PdfDocEncoding encoded string. If unsupported characters are encountered
         /// meaning we cannot safely round-trip the value to bytes this will instead return false.
         /// </summary>
-        public static bool TryConvertBytesToString(byte[] bytes, out string result)
+        public static bool TryConvertBytesToString(ReadOnlySpan<byte> bytes, out string result)
         {
             result = null;
             if (bytes.Length == 0)
