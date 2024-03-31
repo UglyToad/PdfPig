171520abdfb462f0f9c502d85151754b6fee3a35
Jason Nelson <jason@carbonmade.com>
2024-03-30 19:20:08 -0700

    Avoid allocation when passing an empty span

--- src/UglyToad.PdfPig/Polyfills/EncodingExtensions.cs
+++ src/UglyToad.PdfPig/Polyfills/EncodingExtensions.cs
@@ -6,8 +6,13 @@ internal static class EncodingExtensions
 {
     public static string GetString(this Encoding encoding, ReadOnlySpan<byte> bytes)
     {
+        if (bytes.IsEmpty)
+        {
+            return string.Empty;
+        }
+
         // NOTE: this can be made allocation free by introducing unsafe
-       
+
         return encoding.GetString(bytes.ToArray());
     }
 }
