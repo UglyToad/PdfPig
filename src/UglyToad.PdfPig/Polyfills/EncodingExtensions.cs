63c54ccff34da75307439f7fa89007553a09d76a
Jason Nelson <jason@carbonmade.com>
2024-03-30 19:19:07 -0700

    Remove unsafe code

--- src/UglyToad.PdfPig/Polyfills/EncodingExtensions.cs
+++ src/UglyToad.PdfPig/Polyfills/EncodingExtensions.cs
@@ -4,12 +4,11 @@ namespace System.Text;
 
 internal static class EncodingExtensions
 {
-    public unsafe static string GetString(this Encoding encoding, ReadOnlySpan<byte> bytes)
+    public static string GetString(this Encoding encoding, ReadOnlySpan<byte> bytes)
     {
-        fixed (byte* pBytes = bytes)
-        {
-            return encoding.GetString(pBytes, bytes.Length);
-        }
+        // NOTE: this can be made allocation free by introducing unsafe
+       
+        return encoding.GetString(bytes.ToArray());
     }
 }
 
