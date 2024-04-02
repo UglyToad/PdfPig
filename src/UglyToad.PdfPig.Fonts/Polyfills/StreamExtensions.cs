fba5b6071873b33418d12a52b136260dab16a69c
Jason Nelson <jason@carbonmade.com>
2024-04-01 21:43:44 -0700

    [Fonts] Add Stream.Write(Span<byte>) polyfill

---
+++ src/UglyToad.PdfPig.Fonts/Polyfills/StreamExtensions.cs
@@ -0,0 +1,26 @@
+﻿#if NETFRAMEWORK || NETSTANDARD2_0
+
+namespace System.IO;
+
+using System.Buffers;
+
+internal static class StreamExtensions
+{
+    public static void Write(this Stream stream, ReadOnlySpan<byte> buffer)
+    {
+        var tempBuffer = ArrayPool<byte>.Shared.Rent(buffer.Length);
+
+        buffer.CopyTo(tempBuffer);
+
+        try
+        {
+            stream.Write(tempBuffer, 0, buffer.Length);
+        }
+        finally
+        {
+            ArrayPool<byte>.Shared.Return(tempBuffer);
+        }
+    }
+}
+
+#endif
