a68febfa95fb4789576ecd31e760158ab14dfa4f
Jason Nelson <jason@carbonmade.com>
2024-04-03 22:02:06 -0700

    React to IPdfImage change

--- src/UglyToad.PdfPig.Tests/TestPdfImage.cs
+++ src/UglyToad.PdfPig.Tests/TestPdfImage.cs
@@ -19,6 +19,8 @@
 
         public ReadOnlyMemory<byte> RawMemory { get; }
 
+        public ReadOnlySpan<byte> RawBytes => RawMemory.Span;
+
         public RenderingIntent RenderingIntent { get; set; } = RenderingIntent.RelativeColorimetric;
 
         public bool IsImageMask { get; set; }
