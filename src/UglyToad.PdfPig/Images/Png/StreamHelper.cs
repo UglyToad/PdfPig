8314f411764d81ceb9f282214f63645b4b13b4c9
Jason Nelson <jason@carbonmade.com>
2024-04-03 22:31:47 -0700

    Remove unused StreamHelper methods

--- src/UglyToad.PdfPig/Images/Png/StreamHelper.cs
+++ src/UglyToad.PdfPig/Images/Png/StreamHelper.cs
@@ -1,34 +1,18 @@
 ﻿namespace UglyToad.PdfPig.Images.Png
 {
     using System;
+    using System.Buffers.Binary;
     using System.IO;
 
     internal static class StreamHelper
     {
-        public static int ReadBigEndianInt32(Stream stream)
-        {
-            return (ReadOrTerminate(stream) << 24) + (ReadOrTerminate(stream) << 16)
-                                                   + (ReadOrTerminate(stream) << 8) + ReadOrTerminate(stream);
-        }
-
         public static void WriteBigEndianInt32(Stream stream, int value)
         {
-            stream.WriteByte((byte)(value >> 24));
-            stream.WriteByte((byte)(value >> 16));
-            stream.WriteByte((byte)(value >> 8));
-            stream.WriteByte((byte)value);
-        }
+            Span<byte> buffer = stackalloc byte[4];
 
-        private static byte ReadOrTerminate(Stream stream)
-        {
-            var b = stream.ReadByte();
+            BinaryPrimitives.WriteInt32BigEndian(buffer, value);
 
-            if (b == -1)
-            {
-                throw new InvalidOperationException($"Unexpected end of stream at {stream.Position}.");
-            }
-
-            return (byte) b;
+            stream.Write(buffer);
         }
 
         public static bool TryReadHeaderBytes(Stream stream, out byte[] bytes)
