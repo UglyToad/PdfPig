9586b90f22ff5a00d0b76cb6fc47bf6406a3f202
Jason Nelson <jason@carbonmade.com>
2024-04-12 10:39:17 -0700

    Introduce StringSplitter and eliminate various allocations in GlyphListFactory

--- src/UglyToad.PdfPig.Fonts/GlyphListFactory.cs
+++ src/UglyToad.PdfPig.Fonts/GlyphListFactory.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
+    using Util;
 
     internal class GlyphListFactory
     {
@@ -34,6 +35,8 @@
             return ReadInternal(stream);
         }
 
+        private static readonly char[] Semicolon = [';'];
+
         private static GlyphList ReadInternal(Stream stream, int? defaultDictionaryCapacity = 0)
         {
             if (stream == null)
@@ -41,8 +44,8 @@
                 throw new ArgumentNullException(nameof(stream));
             }
 
-            var result = defaultDictionaryCapacity.HasValue ? new Dictionary<string, string>(defaultDictionaryCapacity.Value)
-                    : new Dictionary<string, string>();
+            var result = defaultDictionaryCapacity.HasValue ? new Dictionary<string, string>(defaultDictionaryCapacity.Value) : [];
+
 
             using (var reader = new StreamReader(stream))
             {
@@ -60,7 +63,7 @@
                         continue;
                     }
 
-                    var parts = line.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    var parts = line.Split(Semicolon, StringSplitOptions.RemoveEmptyEntries);
 
                     if (parts.Length != 2)
                     {
@@ -70,13 +73,16 @@
 
                     var key = parts[0];
 
-                    var values = parts[1].Split(' ');
-
+                    var valueReader = new StringSplitter(parts[1].AsSpan(), ' ');
                     var value = string.Empty;
-                    foreach (var s in values)
-                    {
-                        var code = int.Parse(s, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
 
+                    while (valueReader.TryRead(out var s))
+                    {
+#if NET6_0_OR_GREATER
+                        var code = int.Parse(s, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+#else
+                        var code = int.Parse(s.ToString(), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+#endif
                         value += char.ConvertFromUtf32(code);
                     }
 
---
+++ src/UglyToad.PdfPig.Fonts/Util/StringSplitter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace UglyToad.PdfPig.Util;
+
+internal ref struct StringSplitter(ReadOnlySpan<char> text, char separator)
+{
+    private readonly ReadOnlySpan<char> text = text;
+    private readonly char separator = separator;
+    private int position = 0;
+
+    public bool TryRead(out ReadOnlySpan<char> result)
+    {
+        if (IsEof)
+        {
+            result = default;
+
+            return false;
+        }
+
+        int separatorIndex = text.Slice(position).IndexOf(separator);
+
+        if (separatorIndex > -1)
+        {
+            result = text.Slice(position, separatorIndex);
+
+            position += separatorIndex + 1;
+        }
+        else
+        {
+            result = text.Slice(position);
+
+            position = text.Length;
+        }
+
+        return true;
+    }
+
+    public ReadOnlySpan<char> Read()
+    {
+        if (IsEof)
+        {
+            ThrowEof();
+        }
+
+        int start = position;
+
+        int separatorIndex = text.Slice(position).IndexOf(separator);
+
+        if (separatorIndex > -1)
+        {
+            position += separatorIndex + 1;
+
+            return text.Slice(start, separatorIndex);
+        }
+        else
+        {
+            position = text.Length;
+
+            return text.Slice(start);
+        }
+    }
+
+    public readonly bool IsEof => position == text.Length;
+
+    private static void ThrowEof()
+    {
+        throw new EndOfStreamException();
+    }
+}
