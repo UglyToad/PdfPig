8ce6bcc2a795bfb190a2357a8275d6a6c11aca90
BobLd <38405645+BobLd@users.noreply.github.com>
2024-08-25 11:59:19 +0100

    Fix ByteEncodingCMapTable.CharacterCodeToGlyphIndex() to account for glyph mapping length, add test and fix #881

---
+++ src/UglyToad.PdfPig.Tests/Integration/TrueTypeTests.cs
@@ -0,0 +1,24 @@
+﻿namespace UglyToad.PdfPig.Tests.Integration
+{
+    using System.Linq;
+
+    public class TrueTypeTests
+    {
+        [Fact]
+        public void Issue881()
+        {
+            var file = IntegrationHelpers.GetDocumentPath("issue_881.pdf");
+
+            using (var document = PdfDocument.Open(file))
+            {
+                var page = document.GetPage(1);
+                var words = page.GetWords().ToArray();
+                Assert.Equal(4, words.Length);
+                Assert.Equal("IDNR:", words[0].Text);
+                Assert.Equal("4174", words[1].Text);
+                Assert.Equal("/", words[2].Text);
+                Assert.Equal("06.08.2018", words[3].Text);
+            }
+        }
+    }
+}
