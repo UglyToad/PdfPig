f6566d6007a11578c2127960a0c0f92f5020a8e2
BobLd <38405645+BobLd@users.noreply.github.com>
2024-09-17 07:02:29 +0100

    Fix #878

--- src/UglyToad.PdfPig/Graphics/Operations/OperationWriteHelper.cs
+++ src/UglyToad.PdfPig/Graphics/Operations/OperationWriteHelper.cs
@@ -2,26 +2,31 @@
 {
     using PdfPig.Core;
     using System;
+    using System.Buffers;
     using System.Buffers.Text;
+    using System.Globalization;
     using System.IO;
-    using System.Text;
     using Util;
 
     internal static class OperationWriteHelper
     {
         private const byte Whitespace = (byte)' ';
         private const byte NewLine = (byte)'\n';
+        private const byte Zero = (byte)'0';
+        private const byte Point = (byte)'.';
+
+        private static readonly StandardFormat StandardFormatDouble = new StandardFormat('F', 9);
 
         public static void WriteText(this Stream stream, string text, bool appendWhitespace = false)
         {
 #if NET8_0_OR_GREATER
-            if (Ascii.IsValid(text))
+            if (System.Text.Ascii.IsValid(text))
             {
                 Span<byte> buffer = text.Length <= 64
                     ? stackalloc byte[text.Length]
                     : new byte[text.Length];
 
-                Ascii.FromUtf16(text, buffer, out _);
+                System.Text.Ascii.FromUtf16(text, buffer, out _);
 
                 stream.Write(buffer);
             }
@@ -75,11 +80,57 @@
 
         public static void WriteDouble(this Stream stream, double value)
         {
-            Span<byte> buffer = stackalloc byte[32]; // matches dotnet Number.CharStackBufferSize
+            int stackSize = 32; // matches dotnet Number.CharStackBufferSize
 
-            Utf8Formatter.TryFormat(value, buffer, out int bytesWritten);
+            bool success = TryWriteDouble(stream, value, stackSize);
+            while (!success && stackSize <= 1024)
+            {
+                stackSize *= 2;
+                success = TryWriteDouble(stream, value, stackSize);
+            }
 
-            stream.Write(buffer.Slice(0, bytesWritten));
+            if (!success)
+            {
+                ReadOnlySpan<byte> buffer = System.Text.Encoding.UTF8.GetBytes(value.ToString("F9", CultureInfo.InvariantCulture));
+                int lastIndex = GetLastSignificantDigitIndex(buffer, buffer.Length);
+                stream.Write(buffer.Slice(0, lastIndex));
+            }
+        }
+
+        private static bool TryWriteDouble(Stream stream, double value, int stackSize)
+        {
+            System.Diagnostics.Debug.Assert(stackSize <= 1024);
+
+            Span<byte> buffer = stackalloc byte[stackSize];
+
+            if (Utf8Formatter.TryFormat(value, buffer, out int bytesWritten, StandardFormatDouble))
+            {
+                int lastIndex = GetLastSignificantDigitIndex(buffer, bytesWritten);
+                stream.Write(buffer.Slice(0, lastIndex));
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int GetLastSignificantDigitIndex(ReadOnlySpan<byte> buffer, int bytesWritten)
+        {
+            int lastIndex = bytesWritten;
+            for (int i = bytesWritten - 1; i > 1; --i)
+            {
+                if (buffer[i] != Zero)
+                {
+                    break;
+                }
+                lastIndex--;
+            }
+
+            if (buffer[lastIndex - 1] == Point)
+            {
+                lastIndex--;
+            }
+
+            return lastIndex;
         }
 
         public static void WriteNumberText(this Stream stream, int number, string text)
