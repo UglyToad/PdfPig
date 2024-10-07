a258090e1c835f77e5ed80b1d6fe76eac15dc756
BobLd <38405645+BobLd@users.noreply.github.com>
2024-10-07 06:17:00 +0100

    Fix GetTextOrientation by cleanly checking if rotation is divisible by 90 and fix #913

--- src/UglyToad.PdfPig.Tests/Integration/GithubIssuesTests.cs
+++ src/UglyToad.PdfPig.Tests/Integration/GithubIssuesTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace UglyToad.PdfPig.Tests.Integration
 {
+    using Content;
+
     public class GithubIssuesTests
     {
         [Fact]
@@ -20,6 +22,33 @@
             {
                 var ex = Assert.Throws<ArgumentNullException>(() => document.GetPage(1));
                 Assert.StartsWith("Value cannot be null.", ex.Message);
+            }
+        }
+
+        [Fact]
+        public void Issue913()
+        {
+            var doc = IntegrationHelpers.GetSpecificTestDocumentPath("Rotation 45.pdf");
+
+            using (var document = PdfDocument.Open(doc))
+            {
+                var page1 = document.GetPage(1);
+
+                for (int l = 131; l <= 137; ++l)
+                {
+                    var letter = page1.Letters[l];
+                    Assert.Equal(TextOrientation.Other, letter.TextOrientation);
+                    Assert.Equal(45.0, letter.GlyphRectangle.Rotation, 5);
+                }
+
+                var page2 = document.GetPage(2);
+                Assert.Equal(157, page2.Letters.Count);
+
+                var page3 = document.GetPage(3);
+                Assert.Equal(283, page3.Letters.Count);
+
+                var page4 = document.GetPage(4);
+                Assert.Equal(304, page4.Letters.Count);
             }
         }
     }
