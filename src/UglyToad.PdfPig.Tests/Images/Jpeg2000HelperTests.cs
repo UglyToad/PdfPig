204f488ebf1eebdc6ce9047786890d377e8a9ac3
BobLd <38405645+BobLd@users.noreply.github.com>
2025-03-09 13:55:36 +0000

    Improve Jpeg2000Helper to support J2K codec and add test

--- src/UglyToad.PdfPig.Tests/Images/Jpeg2000HelperTests.cs
+++ src/UglyToad.PdfPig.Tests/Images/Jpeg2000HelperTests.cs
@@ -2,12 +2,13 @@
 {
     using PdfPig.Images;
     using System;
+    using UglyToad.PdfPig.Tests.Integration;
 
     public class Jpeg2000HelperTests
     {
         private static readonly Lazy<string> DocumentFolder = new Lazy<string>(() => Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "Images", "Files", "Jpx")));
 
-        public static IEnumerable<object[]> GetAllDocuments
+        public static IEnumerable<object[]> GetAllJp2Files
         {
             get
             {
@@ -15,24 +16,40 @@
             }
         }
 
+        [Theory]
+        [MemberData(nameof(GetAllJp2Files))]
+        public void GetJp2BitsPerComponent_ReturnsCorrectBitsPerComponent_WhenValidInput(string path)
+        {
+            byte[] image = File.ReadAllBytes(Path.Combine(DocumentFolder.Value, path));
+            Assert.Equal(8, Jpeg2000Helper.GetBitsPerComponent(image));
+        }
+
         [Fact]
         public void GetJp2BitsPerComponent_ThrowsException_WhenInputIsTooShort()
         {
-            Assert.Throws<InvalidOperationException>(() => Jpeg2000Helper.GetJp2BitsPerComponent(new byte[11]));
+            Assert.Throws<InvalidOperationException>(() => Jpeg2000Helper.GetBitsPerComponent(new byte[11]));
         }
         
         [Fact]
         public void GetJp2BitsPerComponent_ThrowsException_WhenSignatureBoxIsInvalid()
         {
-            Assert.Throws<InvalidOperationException>(() => Jpeg2000Helper.GetJp2BitsPerComponent(new byte[12]));
+            Assert.Throws<InvalidOperationException>(() => Jpeg2000Helper.GetBitsPerComponent(new byte[12]));
         }
 
-        [Theory]
-        [MemberData(nameof(GetAllDocuments))]
-        public void GetJp2BitsPerComponent_ReturnsCorrectBitsPerComponent_WhenValidInput(string path)
+        [Fact]
+        public void GetJp2BitsPerComponentJ2K()
         {
-            byte[] image = File.ReadAllBytes(Path.Combine(DocumentFolder.Value, path));
-            Assert.Equal(8, Jpeg2000Helper.GetJp2BitsPerComponent(image));
+            string path = IntegrationHelpers.GetSpecificTestDocumentPath("GHOSTSCRIPT-688999-2.pdf");
+
+            using (var document = PdfDocument.Open(path))
+            {
+                var page1 = document.GetPage(1);
+                var jpxImage = page1.GetImages().Single();
+
+                var bpc = Jpeg2000Helper.GetBitsPerComponent(jpxImage.RawBytes);
+
+                Assert.Equal(8, bpc);
+            }
         }
     }
 }
