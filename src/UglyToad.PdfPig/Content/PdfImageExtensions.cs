3a9881e2f11ad8a25d1697c41c4a4838bbb5d2f8
BobLd <38405645+BobLd@users.noreply.github.com>
2025-05-11 15:42:51 +0100

    Add early support for Stencil masking, rename SoftMaskImage property into MaskImage and make sure IsInlineImage is true for InlineImage

---
+++ src/UglyToad.PdfPig/Content/PdfImageExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace UglyToad.PdfPig.Content
+{
+    /// <summary>
+    /// Pdf image extensions.
+    /// </summary>
+    public static class PdfImageExtensions
+    {
+        /// <summary>
+        /// <c>true</c> if the image colors needs to be reversed based on the Decode array and color space. <c>false</c> otherwise.
+        /// </summary>
+        public static bool NeedsReverseDecode(this IPdfImage pdfImage)
+        {
+            if (pdfImage.ColorSpaceDetails?.IsStencil == true)
+            {
+                // Stencil color space already takes care of reversing.
+                return false;
+            }
+
+            return pdfImage.Decode.Count >= 2 && pdfImage.Decode[0] == 1 && pdfImage.Decode[1] == 0;
+        }
+    }
+}
