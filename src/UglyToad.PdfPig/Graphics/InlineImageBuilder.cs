UglyToad/PdfPig

commit 4dab2ef2392a19b9548ca5f073acd4b626357de7

Add early support for Stencil masking, rename SoftMaskImage property into MaskImage and make sure IsInlineImage is true for InlineImage

src/UglyToad.PdfPig/XObjects/XObjectFactory.cs
@@ -63,12 +63,30 @@
                     throw new Exception("The SMask dictionary contains a 'Mask' or 'Smask' entry.");
                 }
 
-                var renderingIntent = xObject.DefaultRenderingIntent; // Ignored
-
-                XObjectContentRecord softMaskImageRecord = new XObjectContentRecord(XObjectType.Image, sMaskToken, TransformationMatrix.Identity,
-                    renderingIntent, DeviceGrayColorSpaceDetails.Instance);
+                XObjectContentRecord softMaskImageRecord = new XObjectContentRecord(XObjectType.Image,
+                    sMaskToken,
+                    TransformationMatrix.Identity,
+                    xObject.DefaultRenderingIntent, // Ignored
+                    DeviceGrayColorSpaceDetails.Instance);
 
                 softMaskImage = ReadImage(softMaskImageRecord, pdfScanner, filterProvider, resourceStore);
+            }
+            else if (dictionary.TryGet(NameToken.Mask, out StreamToken maskStream))
+            {
+                if (maskStream.StreamDictionary.ContainsKey(NameToken.ColorSpace))
+                {
+                    throw new Exception("The SMask dictionary contains a 'ColorSpace'.");
+                }
+
+                // Stencil masking
+                XObjectContentRecord maskImageRecord = new XObjectContentRecord(XObjectType.Image,
+                    maskStream,
+                    TransformationMatrix.Identity,
+                    xObject.DefaultRenderingIntent,
+                    null);
+
+                softMaskImage = ReadImage(maskImageRecord, pdfScanner, filterProvider, resourceStore);
+                System.Diagnostics.Debug.Assert(softMaskImage.ColorSpaceDetails?.IsStencil == true);
             }
 
             var isJpxDecode = dictionary.TryGet(NameToken.Filter, out NameToken filterName) && filterName.Equals(NameToken.JpxDecode);
@@ -125,9 +143,7 @@
                 }
             }
 
-            var streamToken = new StreamToken(dictionary, xObject.Stream.Data);
-
-            var decodedBytes = supportsFilters ? new Lazy<ReadOnlyMemory<byte>>(() => streamToken.Decode(filterProvider, pdfScanner))
+            var decodedBytes = supportsFilters ? new Lazy<ReadOnlyMemory<byte>>(() => xObject.Stream.Decode(filterProvider, pdfScanner))
                 : null;
 
             var decode = Array.Empty<double>();

src/UglyToad.PdfPig.Tests/Integration/MaskTests.cs
@@ -3,7 +3,7 @@
     using SkiaSharp;
     using System.Linq;
 
-    public class SoftMaskTests
+    public class MaskTests
     {
         [Fact]
         public void PigProductionHandbook()
@@ -17,7 +17,7 @@
                 var images = page.GetImages().ToArray();
 
                 var image1 = images[1];
-                Assert.NotNull(image1.SoftMaskImage);
+                Assert.NotNull(image1.MaskImage);
                 Assert.True(image1.TryGetPng(out var png1));
                 using (var skImage1 = SKImage.FromEncodedData(png1))
                 using (var skBitmap1 = SKBitmap.FromImage(skImage1))
@@ -27,7 +27,7 @@
                 }
 
                 var image2 = images[2];
-                Assert.NotNull(image2.SoftMaskImage);
+                Assert.NotNull(image2.MaskImage);
                 Assert.True(image2.TryGetPng(out var png2));
                 using (var skImage2 = SKImage.FromEncodedData(png2))
                 using (var skBitmap2 = SKBitmap.FromImage(skImage2))
@@ -35,6 +35,38 @@
                     var pixel = skBitmap2.GetPixel(0, 0);
                     Assert.Equal(0, pixel.Alpha);
                 }
+            }
+        }
+
+        [Fact]
+        public void MOZILLA_LINK_3264_0()
+        {
+            var path = IntegrationHelpers.GetDocumentPath("MOZILLA-LINK-3264-0.pdf");
+
+            using (var document = PdfDocument.Open(path, new ParsingOptions() { UseLenientParsing = true, SkipMissingFonts = true }))
+            {
+                var page = document.GetPage(1);
+
+                var images = page.GetImages().ToArray();
+
+                var image1 = images[1];
+                Assert.NotNull(image1.MaskImage);
+                Assert.True(image1.TryGetPng(out var png1));
+                using (var skImage1 = SKImage.FromEncodedData(png1))
+                using (var skBitmap1 = SKBitmap.FromImage(skImage1))
+                {
+                    var pixel = skBitmap1.GetPixel(0, 0);
+                    Assert.Equal(0, pixel.Alpha);
+                }
+
+                page = document.GetPage(2);
+
+                images = page.GetImages().ToArray();
+
+                var image2 = images[1];
+                Assert.NotNull(image2.MaskImage);
+                Assert.True(image2.TryGetPng(out var png2));
+                // TODO - Check alpha value
             }
         }
     }

src/UglyToad.PdfPig.Tests/PublicApiScannerTests.cs
@@ -80,6 +80,7 @@
                 "UglyToad.PdfPig.Content.InlineImage",
                 "UglyToad.PdfPig.Content.IPageFactory`1",
                 "UglyToad.PdfPig.Content.IPdfImage",
+                "UglyToad.PdfPig.Content.PdfImageExtensions",
                 "UglyToad.PdfPig.Content.IResourceStore",
                 "UglyToad.PdfPig.Content.Letter",
                 "UglyToad.PdfPig.Content.MarkedContentElement",

src/UglyToad.PdfPig.Tests/TestPdfImage.cs
@@ -37,7 +37,7 @@
 
         public ReadOnlyMemory<byte> DecodedBytes { get; set; }
 
-        public IPdfImage? SoftMaskImage { get; }
+        public IPdfImage? MaskImage { get; }
 
         public bool TryGetBytesAsMemory(out ReadOnlyMemory<byte> bytes)
         {

src/UglyToad.PdfPig/Content/IPdfImage.cs
@@ -95,9 +95,10 @@
         ColorSpaceDetails? ColorSpaceDetails { get; }
 
         /// <summary>
-        /// Soft-mask image.
+        /// The image mask.
+        /// <para>Either a Soft-mask or a Stencil mask.</para>
         /// </summary>
-        IPdfImage? SoftMaskImage { get; }
+        IPdfImage? MaskImage { get; }
 
         /// <summary>
         /// Get the decoded memory of the image if applicable. For JPEG images and some other types the

src/UglyToad.PdfPig/Content/InlineImage.cs
@@ -58,7 +58,7 @@
         public ColorSpaceDetails ColorSpaceDetails { get; }
 
         /// <inheritdoc />
-        public IPdfImage? SoftMaskImage { get; }
+        public IPdfImage? MaskImage { get; }
 
         /// <summary>
         /// Create a new <see cref="InlineImage"/>.
@@ -78,6 +78,7 @@
             ColorSpaceDetails colorSpaceDetails,
             IPdfImage? softMaskImage)
         {
+            IsInlineImage = true;
             Bounds = bounds;
             WidthInSamples = widthInSamples;
             HeightInSamples = heightInSamples;
@@ -114,7 +115,7 @@
                 return b;
             }) : null;
 
-            SoftMaskImage = softMaskImage;
+            MaskImage = softMaskImage;
         }
 
         /// <inheritdoc />

src/UglyToad.PdfPig/Content/PdfImageExtensions.cs
@@ -1,0 +1,22 @@
+﻿namespace UglyToad.PdfPig.Content
+{
+    /// <summary>
+    /// Pdf image extensions.
+    /// </summary>
+    public static class PdfImageExtensions
+    {
+        /// <summary>
+        /// <c>true</c> if the image colors needs to be reversed based on the Decode array and color space. <c>false</c> otherwise.
+        /// </summary>
+        public static bool NeedsReverseDecode(this IPdfImage pdfImage)
+        {
+            if (pdfImage.ColorSpaceDetails?.IsStencil == true)
+            {
+                // Stencil color space already takes care of reversing.
+                return false;
+            }
+
+            return pdfImage.Decode.Count >= 2 && pdfImage.Decode[0] == 1 && pdfImage.Decode[1] == 0;
+        }
+    }
+}

commit 9b2b5c43fe81d0fe7a33dc74ccdf8dcf9fc2e061

Fix bug introduced in #1039

src/UglyToad.PdfPig/Graphics/InlineImageBuilder.cs
@@ -55,7 +55,7 @@
             var bitsPerComponent = GetByKeys<NumericToken>(NameToken.BitsPerComponent, NameToken.Bpc, !isMask)?.Int ?? 1;
             NameToken? colorSpaceName = null;
 
-            var imgDic = new DictionaryToken(Properties ?? new Dictionary<NameToken, IToken>());
+            var imgDic = new DictionaryToken(Properties ?? new Dictionary<NameToken, IToken>()).Resolve(tokenScanner);
 
             XObjectImage? softMaskImage = null;
             if (imgDic.TryGet(NameToken.Smask, tokenScanner, out StreamToken? sMaskToken))

src/UglyToad.PdfPig/XObjects/XObjectFactory.cs
@@ -142,8 +142,9 @@
                     break;
                 }
             }
-
-            var decodedBytes = supportsFilters ? new Lazy<ReadOnlyMemory<byte>>(() => xObject.Stream.Decode(filterProvider, pdfScanner))
+            
+            var streamToken = new StreamToken(dictionary, xObject.Stream.Data); // Needed as Resolve(pdfScanner) was called on the dictionary
+            var decodedBytes = supportsFilters ? new Lazy<ReadOnlyMemory<byte>>(() => streamToken.Decode(filterProvider, pdfScanner))
                 : null;
 
             var decode = Array.Empty<double>();