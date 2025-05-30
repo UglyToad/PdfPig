662d5061d19a268447c66204e613152f12eb8fc1
BobLd <38405645+BobLd@users.noreply.github.com>
2025-05-30 11:27:02 +0100

    Add extension method to get Memory<byte> from MemoryStream, attempting to do it without allocation and update CMapParser

--- src/UglyToad.PdfPig.Core/MemoryHelper.cs
+++ src/UglyToad.PdfPig.Core/MemoryHelper.cs
@@ -10,7 +10,7 @@
     public static class MemoryHelper
     {
         /// <summary>
-        /// Gets a read-only <see cref="MemoryStream"/> from a ReadOnlyMemory&lt;byte&gt;.
+        /// Gets a read-only <see cref="MemoryStream"/> from a ReadOnlyMemory&lt;byte&gt;, attempting without memory allocation.
         /// </summary>
         public static MemoryStream AsReadOnlyMemoryStream(this ReadOnlyMemory<byte> memory)
         {
@@ -20,6 +20,19 @@
             }
 
             return new MemoryStream(memory.ToArray(), false);
+        }
+
+        /// <summary>
+        /// Gets the Memory&lt;byte&gt; from a <see cref="MemoryStream"/>, attempting without memory allocation.
+        /// </summary>
+        public static Memory<byte> AsMemory(this MemoryStream stream)
+        {
+            if (stream.TryGetBuffer(out ArraySegment<byte> segment))
+            {
+                return segment.AsMemory();
+            }
+
+            return stream.ToArray();
         }
     }
 }
