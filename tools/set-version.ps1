73d3bca7efb127d12bebdeda4e6019af7aa9a1ad
EliotJones <elioty@hotmail.co.uk>
2025-09-07 15:52:26 +0200

    enable setting assembly and file version

--- tools/set-version.ps1
+++ tools/set-version.ps1
@@ -1,6 +1,8 @@
 param (
   [Parameter(Position = 0, mandatory = $true)]
-  [string]$version
+  [string]$version,
+
+  [switch]$UpdateAssemblyAndFileVersion
 )
 
 $root = (Split-Path -parent $PSCommandPath)
@@ -17,6 +19,26 @@ $packageProjectPath = "$root/UglyToad.PdfPig.Package/UglyToad.PdfPig.Package.csp
 $xml = New-Object XML
 $xml.Load($packageProjectPath)
 $xml.Project.PropertyGroup[0].Version = $version
+
+if ($UpdateAssemblyAndFileVersion) {
+    # Update AssemblyVersion and FileVersion if the nodes exist, otherwise create them
+    if (-not $xml.Project.PropertyGroup[0].AssemblyVersion) {
+        $node = $xml.CreateElement("AssemblyVersion")
+        $node.InnerText = "$version.0"  # add the 4th segment
+        $xml.Project.PropertyGroup[0].AppendChild($node) | Out-Null
+    } else {
+        $xml.Project.PropertyGroup[0].AssemblyVersion = "$version.0"
+    }
+
+    if (-not $xml.Project.PropertyGroup[0].FileVersion) {
+        $node = $xml.CreateElement("FileVersion")
+        $node.InnerText = "$version.0"
+        $xml.Project.PropertyGroup[0].AppendChild($node) | Out-Null
+    } else {
+        $xml.Project.PropertyGroup[0].FileVersion = "$version.0"
+    }
+}
+
 $xml.Save($packageProjectPath)
 
 Write-Host $projs.Length
