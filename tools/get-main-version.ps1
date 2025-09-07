024efc2eb16076306dc3d1c236bb167b3467abb3
EliotJones <elioty@hotmail.co.uk>
2025-09-07 15:17:41 +0200

    remove alpha postfix, releases will increment version

--- tools/get-main-version.ps1
+++ tools/get-main-version.ps1
@@ -1,5 +1,6 @@
 $xml = New-Object XML
-$xml.Load(".\tools\UglyToad.PdfPig.Package\UglyToad.PdfPig.Package.csproj")
+$projectPath = Join-Path $PSScriptRoot "UglyToad.PdfPig.Package\UglyToad.PdfPig.Package.csproj"
+$xml.Load($projectPath)
 $current = $xml.Project.PropertyGroup[0].Version
 $hyphenIndex = $current.IndexOf('-')
 $len = If ($hyphenIndex -lt 0) { $current.Length } Else { $hyphenIndex }
