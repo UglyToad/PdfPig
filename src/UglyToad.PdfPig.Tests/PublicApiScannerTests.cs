2fc926a5640f7d42d1fff0678ff7e2170570e16f
davmarksman <david@brokit.co.uk>
2026-02-26 08:02:13 +0000

    Minor fix

--- src/UglyToad.PdfPig.Tests/PublicApiScannerTests.cs
+++ src/UglyToad.PdfPig.Tests/PublicApiScannerTests.cs
@@ -78,7 +78,6 @@
                 "UglyToad.PdfPig.Content.EmbeddedFile",
                 "UglyToad.PdfPig.Content.Hyperlink",
                 "UglyToad.PdfPig.Content.IBoundingBox",
-                "UglyToad.PdfPig.Content.ILettersBlock",
                 "UglyToad.PdfPig.Content.InlineImage",
                 "UglyToad.PdfPig.Content.IPageFactory`1",
                 "UglyToad.PdfPig.Content.IPdfImage",
