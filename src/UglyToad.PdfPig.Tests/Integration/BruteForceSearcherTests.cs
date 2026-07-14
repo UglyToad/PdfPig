0d0f7bd53752e2245d3b73d8a2efbe4791d94e1e
BobLd <38405645+BobLd@users.noreply.github.com>
2026-07-14 14:51:12 +0100

    Add integration test for 28a1878 (#1360)

---
+++ src/UglyToad.PdfPig.Tests/Integration/BruteForceSearcherTests.cs
@@ -0,0 +1,19 @@
+﻿namespace UglyToad.PdfPig.Tests.Integration
+{
+    public class BruteForceSearcherTests
+    {
+        [Fact]
+        public void Pull1360()
+        {
+            // See https://github.com/UglyToad/PdfPig/pull/1360
+            var path = IntegrationHelpers.GetSpecificTestDocumentPath("pdfium_type3_basic.pdf");
+            using (var document = PdfDocument.Open(path, new ParsingOptions() { UseLenientParsing = true }))
+            {
+                foreach (var page in document.GetPages())
+                {
+                    Assert.NotNull(page);
+                }
+            }
+        }
+    }
+}
