b365f0602939c1cfe305ef6de435abc806da3937
BobLd <38405645+BobLd@users.noreply.github.com>
2026-08-20 17:46:12 +0100

    Include time in nightly version date format

--- tools/generate-nightly-version.ps1
+++ tools/generate-nightly-version.ps1
@@ -2,6 +2,6 @@ $toolsRoot = (Split-Path -parent $PSCommandPath)
 
 $mainVersion = & "$toolsRoot\get-main-version.ps1"
 $commitHash = (git rev-parse HEAD).Substring(0, 5)
-$date = [System.DateTime]::UtcNow.ToString('yyyyMMdd')
+$date = [System.DateTime]::UtcNow.ToString('yyyyMMddHHmm')
 $nightlyVersion = "$mainVersion-alpha-$date-$commitHash"
 Write-Output $nightlyVersion
